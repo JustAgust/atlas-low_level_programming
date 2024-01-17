86166e7eb1a9f93d9f2762f2dcc1e5852f80d6c8
JustAgust <charles.sparks@atlasschool.com>
2024-01-17 20:14:25 +0000

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,12 @@
-#!/bin/bash
-gcc -c int main() {
-    puts("Programming is like building a multilingual puzzle");
-    return 0;
+#include <stdio.h>
+
+/**
+ * main - puts
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
