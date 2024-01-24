00de598d03f28c8382dcf1ef717039c589032e19
JustAgust <charles.sparks@atlasschool.com>
2024-01-24 17:48:20 +0000

    4-print_most_numbers.c

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints nums 0 to 9 except 2 and 4 with new line
+ *
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int t;
+
+	for (t = 0; t <= 9; t++)
+	{
+		if (t != 2; t != 4)
+			_putchar(t + '0');
+	}
+		_putchar('\n');
+}
