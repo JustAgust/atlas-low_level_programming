87053517b9da18d90e623511c503c80bec85f6a9
JustAgust <charles.sparks@atlasschool.com>
2024-01-24 19:14:10 +0000

    6-print_line.c

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *
+ * print_line - draw straight line in terminal
+ * @n: length of line
+ * Return: zero
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+		for ( i = 0; i < 0; i++)
+			_putchar('_');
+	_putchar('\n');
+}
