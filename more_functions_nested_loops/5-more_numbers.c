61631658ecb87ffa81a9804dfecd0808c5032a2e
JustAgust <charles.sparks@atlasschool.com>
2024-01-24 18:26:24 +0000

    5-more_numbers.c

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * more_numbers
+ *
+ * Return: null
+ */
+
+void more_numbers(void)
+{
+	int = x;
+	int = z;
+	for (x = 0; x < 10; i++)
+	{
+	for (z = 0; z <= 14; j++)
+	{
+		if (z > 9)
+			_putchar((z / 10) + '0');
+		_putchar((z % 10) + '0');
+	}
+	_putchar('\n');
+	}
+}
+
