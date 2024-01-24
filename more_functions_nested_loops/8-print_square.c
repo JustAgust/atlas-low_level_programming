1d5bfe3f9d1f78b566108909ab3c55aba1873712
JustAgust <charles.sparks@atlasschool.com>
2024-01-24 22:14:24 +0000

    8-print_square.c

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_square - print a square
+ * @size: size of square
+ * Return: zero
+ */
+void print_square(int size)
+{
+	int x = 0, z;
+
+	if (size > 0)
+	{
+		do
+		{
+			z = 0;
+			while (z < size)
+			{
+				_putchar('#');
+				z++;
+			}
+			_putchar('\n');
+			x++;
+		}while (x < size);
+	}
+	else
+		_putchar('\n');
+}
