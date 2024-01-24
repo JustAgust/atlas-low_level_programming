eccbedc3a569489c62ae9985d6b0bbb753d94618
JustAgust <charles.sparks@atlasschool.com>
2024-01-24 17:10:24 +0000

    multply

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _mult multiplies two integers
+ * @a: first interger
+ * @b: second interger
+ * Return value after mul
+ */
+
+int mul(int a, int b)
+{
+	return ( a * b );
+}
