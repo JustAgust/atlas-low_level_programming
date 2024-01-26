1528658a8c6bd066b1083632c299aebcdb9c5659
JustAgust <charles.sparks@atlasschool.com>
2024-01-26 17:54:16 +0000

    2-strlen.c

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * int _strlen - returns string leght
+ * @s: sting length parameter
+ * Return: sting lenth value
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+	
+		while(*s != '\0')
+		{
+			len++;
+			s++;
+		}
+		return (len);
+}
+
