b9be879de40bdde0e4ade1cc99bcf3f95a56aa89
JustAgust <charles.sparks@atlasschool.com>
2024-01-27 10:12:07 +0000

    3-puts.c

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts - prints a string followed by new line
+ * @str: input string
+ */
+void _puts(char *str)
+{
+	while(*str != '\0')
+	{
+		_putschar(*str, str++);
+	}
+	_putschar('\0')
+}
