e3aa2cc49f330067822f66231620ff36948269a1
JustAgust <charles.sparks@atlasschool.com>
2024-01-27 13:24:39 +0000

    4-print_rev.c

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_rev - prints string in rev
+ * @s: string input
+ */
+void print_rev(char *s)
+{
+	int r = 0;
+
+	while (*s != '0')
+	{
+		r++;
+		s++;
+	} 
+	r = r - 1;
+	while (r >= 0)
+	{
+		r--;
+		s--;
+		putchar(*s);
+	}
+	putchar('\n');
+}
