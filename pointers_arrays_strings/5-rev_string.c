35387109e054022bfe90a90d5630b61f7c552f00
JustAgust <charles.sparks@atlasschool.com>
2024-01-28 21:31:37 +0000

    5-rev_string.c

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * rev_string - function that reverses the string
+ * @*s: input string
+ */
+void rev_string(char *s)
+{
+	int k = 0, len = 0;
+	char swap;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	len--;
+	for (; k <= len; k++, len--)
+	{
+		swap = s[len];
+		s[len] = s[k];
+		s[k] = swap;
+	}
+}
