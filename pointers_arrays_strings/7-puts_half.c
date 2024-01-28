63f666ccd544fbf62275bb62c6df5ce8fa399963
JustAgust <charles.sparks@atlasschool.com>
2024-01-28 23:41:32 +0000

    7-puts_half.c

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * puts_half - prints second half of string
+ * @str: input string
+ */
+void puts_half(char *str)
+{
+	int i, n = 0, m = 0;
+	while (str[n] != '\0')
+		n++;
+}
+n--;
+if (n >= 1)
+{
+	m = n / 2 + 1;
+	for (i = m; i <= n; i++)
+	{
+		putchar (str[i]);
+	}
+	putchar ('\n');
+}
