c614c3062a88a71d1fcab947fd6edfdb20fece3d
JustAgust <charles.sparks@atlasschool.com>
2024-01-28 23:29:16 +0000

    6-puts2.c

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts2 - print every other character starting with first, new line
+ * @str: inputs string
+ */
+void puts2(char *str)
+{
+	int len = 0;
+
+	while (*str != '\0')
+	{
+		if (len % 2 == 0)
+			putchar(*str);
+		len++;
+		str++;
+	}
+	putchar('\n')
+}
