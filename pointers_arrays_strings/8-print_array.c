f72d0c87147b146c44adc4b3f7fd2f23164b4bad
JustAgust <charles.sparks@atlasschool.com>
2024-01-29 18:11:30 +0000

    fix merge conflict

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -11,13 +11,9 @@ void print_array(int *a, int n)
 	for (i = 0; i < n; i++)
 	{
 		if (i != (n - 1))
-			_putchar("%d, ", a[i]);
+			printf("%d, ", a[i]);
 		else
-			_putchar("%d", a[i]);
+			printf("%d", a[i]);
 	}
-<<<<<<< HEAD
 	printf("\n");
-=======
-	_putchar('\n');
->>>>>>> parent of 25bc119 (8-print_array.c)
 }
