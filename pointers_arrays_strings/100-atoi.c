41a7644d985ed03c2dac4068285501c8ad1fb897
JustAgust <charles.sparks@atlasschool.com>
2024-01-29 21:41:26 +0000

    try4

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -7,25 +7,21 @@
 int _atoi(char *s) {
     int result = 0;
     int sign = 1;
-    char c;
+    int i;
 
-    
-    while ((c = *s++) != '\0') {
-        if (c == '-')
+    for (i = 0; s[i] != '\0'; i++) {
+        if (s[i] == '-')
             sign *= -1;
-        else if (c >= '0' && c <= '9')
+        else if (s[i] >= '0' && s[i] <= '9')
             break;
     }
 
-    
-    while (c >= '0' && c <= '9') {
-        
+    for (; s[i] >= '0' && s[i] <= '9'; i++) {
         if (result > (sign == -1 ? -(1 << 31) / 10 : ((1 << 31) - 1) / 10)) {
             return sign == -1 ? -(1 << 31) : ((1 << 31) - 1);
         }
-        result = result * 10 + (c - '0');
-        c = *s++;
+        result = result * 10 + (s[i] - '0');
     }
 
-    return (sign * result);
+    return sign * result;
 }
