4a4615bbfa6ec83c3984bed09493b6eb009903f7
JustAgust <charles.sparks@atlasschool.com>
2024-01-29 21:30:45 +0000

    try4

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,37 +1,31 @@
 #include "main.h"
-#include <limits.h>
 /**
  * _atoi - a funtion that converts a string to an integer
  * @s: input string
  * Return: an integer.
  */
-int _atoi(char *s)
-{
-	int i = 0, itg = 0, flag = 1, maxmin = 0;
+int _atoi(char *s) {
+    int result = 0;
+    int sign = 1;
+    char c;
 
-	if (s[0] == '0')
-		itg = 0;
-	else
-	{
-	while ((s[i] != '\0') && itg == 0)
-	{
-		if (s[i] == 45)
-			flag = flag * (-1);
-		while (s[i] >= '0' && s[i] <= '9')
-		{
-			if (!(itg == 214748364 && (int)(s[i] - '0') == 8))
-				itg = itg * 10 + (int)(s[i] - '0');
-			else
-				maxmin = 1;
-			i++;
-		}
-		i++;
-	}
-	}
-	if (maxmin != 1)
-		return (itg * flag);
-	else if (flag > 0)
-		return (INT_MAX);
-	else
-		return (INT_MIN);
+    
+    while ((c = *s++) != '\0') {
+        if (c == '-')
+            sign *= -1;
+        else if (c >= '0' && c <= '9')
+            break;
+    }
+
+    
+    while (c >= '0' && c <= '9') {
+        
+        if (result > (sign == -1 ? -(1 << 31) / 10 : ((1 << 31) - 1) / 10)) {
+            return sign == -1 ? -(1 << 31) : ((1 << 31) - 1);
+        }
+        result = result * 10 + (c - '0');
+        c = *s++;
+    }
+
+    return sign * result;
 }
