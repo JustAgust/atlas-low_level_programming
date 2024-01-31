ee7d4f41a419ac48165b7636f3c85b84e016d81b
JustAgust <charles.sparks@atlasschool.com>
2024-01-31 23:21:17 +0000

    2-strncpy.c

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strncpy - copies string without nullbyte
+ * @dest: string to be replaced
+ * @src: string that will be copied
+ * @n: bytes to be used
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (n > i src[i] != '\0')
+	{
+		dest[i] = src[1];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
