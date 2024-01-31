131f1b21d88e1f2b1858db2c2611a14ef45ed6bc
JustAgust <charles.sparks@atlasschool.com>
2024-01-31 17:19:17 +0000

    strcat

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -8,18 +8,19 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i = 0;
+	int i = 0; int j = 0;
 
-	while (dest[i] != '\0')
+	while (dest[j] != '\0')
 	{
-		i++;
+		j++;
 	}
 	while (src[i] != '\0')
 	{
-		dest[i] = src[i];
+		dest[j] = src[i];
 		i++;
+		j++;
 		
 	}
-	dest[i] = '\0';
+	dest[j] = '\0';
 	return (dest);
 }
