57560947f9440cd8c7ace0c8576e7f67c33fbb93
JustAgust <charles.sparks@atlasschool.com>
2024-02-01 19:22:45 +0000

    5-string_toupper.c

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -6,13 +6,10 @@
  */
 char *string_toupper(char *str)
 {
-	int i = 0;
-
-	while (str[i] != '\0')
+	for ( int i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] >= 97 && str[i] >= 122)
+		if (str[i] >= 'a' && str[i] >= 'z')
 			str[i] = str[i] - 32;
-		i++
 	}
 	return (str);
 }
