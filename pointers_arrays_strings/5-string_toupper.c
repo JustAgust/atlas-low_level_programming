a42127a30892f65056607b6cd462a668abf53443
JustAgust <charles.sparks@atlasschool.com>
2024-02-01 19:08:37 +0000

    5-string_toupper.c

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * string_toupper - turns lower case char into upper case char
+ * @str: input string
+ * Return: all upper case
+ */
+char *string_toupper(char *)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 97 && str[i] >= 122)
+			str[i] = str[i] - 32;
+		i++
+	}
+	return (str);
+}
