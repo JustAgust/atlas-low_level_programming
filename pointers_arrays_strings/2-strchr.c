04b98123a8c83db98bc98801a609841390eda99f
JustAgust <charles.sparks@atlasschool.com>
2024-02-02 20:36:35 +0000

    stringchar

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _strchr - locate char in string
+ * @s: input string
+ * @c: character to find!
+ * Return: pointer upon first occurance
+ */
+char *_strchr(char *s, char c)
+{
+
