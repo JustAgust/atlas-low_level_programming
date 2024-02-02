c2b3c58718e33e1112d3db6e66a29ecb1fc9432d
JustAgust <charles.sparks@atlasschool.com>
2024-02-02 19:16:58 +0000

    set memory

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memset - fills memory with a constant byte
+ * @s: first input string
+ * @b: second input string
+ * @n:unsigned int
+ * Return: returns pointer in area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i = n, i++)
+	{
+		s[b] = n;
+	}
+	return (s)
+}
