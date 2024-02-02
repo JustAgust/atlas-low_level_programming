fbc55923509c06cf8c30ba8ae2589a58ccdde545
JustAgust <charles.sparks@atlasschool.com>
2024-02-02 19:49:12 +0000

    copy memory

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -3,6 +3,7 @@
  * _memcpy - copies n bytes from one area to another
  * @dest: end point
  * @src: start point
+ * @n: copy length
  * Return: pointer to dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
