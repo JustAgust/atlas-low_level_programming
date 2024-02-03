0a52d9d602027e99089b9e2b8e2dc7e078f8151e
JustAgust <charles.sparks@atlasschool.com>
2024-02-03 22:22:37 +0000

    3-strspn.c

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strspn - gets initial segment which consists of specific bytes
+ * @s: intial string segment
+ * @accept: prefix substring
+ * Return: byte number from "s" made of only "accept" bytes
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j, count = 0;
+
+	for (i = 0; s[i] != '\0' && s[i] != " " && s[i] !+ ','; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			count++;
+			break;
+		}
+	}
+}
+return (count);
+}
