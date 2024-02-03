c907acce2511d624384d05864a9b6c1a0ab20255
JustAgust <charles.sparks@atlasschool.com>
2024-02-03 22:28:50 +0000

    3-strspn.c

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -9,16 +9,16 @@ unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i, j, count = 0;
 
-	for (i = 0; s[i] != '\0' && s[i] != ' ' && s[i] !+ ','; i++)
+	for (i = 0; s[i] != '\0' && s[i] != ' ' && s[i] != ','; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 			{
-			count++;
-			break;
+				count++;
+				break;
+			}
 		}
 	}
-}
-return (count);
+	return (count);
 }
