e482b25dd5e7be0ec6546a936a1f7172ab61583a
JustAgust <charles.sparks@atlasschool.com>
2024-02-04 00:40:00 +0000

    4-strpbrk.c

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strpbrk - finds first char in string *s1 that matches any char in *s2
+ * @s: first inout string
+ * @accept: string to be matched
+ * Return: returns pointer to matching char, else returns null.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	char *a;
+
+	while (*s)
+	{
+		a = accept;
+		while (*a)
+		{
+			if (*a == *s)
+				return s;
+			a++;
+		}
+		s++;
+	}
+	return (NULL);
+}
