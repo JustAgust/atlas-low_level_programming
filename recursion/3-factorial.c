052f22874642320d57083529c9fc6fd2b96c3fec
JustAgust <charles.sparks@atlasschool.com>
2024-02-07 19:42:56 +0000

    factorial

---
+++ recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * factorial - returns facorial of num
+ * @n: factorial
+ * Return: -1
+ */
+int factorial(int n)
+{
+	if(n <= 0)
+		return (-1);
+	{
+	return (n * factorial(n - 1);
+			}
+			}
+
