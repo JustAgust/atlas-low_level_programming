7af131c4f1dcbf2228af4ae058e938c17576940d
JustAgust <charles.sparks@atlasschool.com>
2024-02-07 20:28:00 +0000

    powerof

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _pow_recursion - raises to power of
+ * @x: input value
+ * @y: raises input value to power of
+ * Return: value
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	{
+		return _pow_recusrion(x, y);
+	}
+}
