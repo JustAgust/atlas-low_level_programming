174c2b42bac4c47cc6477ca50d4534d53e30b13b
JustAgust <charles.sparks@atlasschool.com>
2024-02-10 23:06:17 +0000

    counts args

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -5,10 +5,10 @@
  * @argv: array of arguments
  * Return: 0
  */
-int main(int argc, char *argv)
+int main(int argc, char **argv)
 {	
 	(void)argv;
 	if (argc >= 1)
-		printf("%d\n", argc -1);
+		printf("%d\n", argc - 1);
 	return (0);
 }
