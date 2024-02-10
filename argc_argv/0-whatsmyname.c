8cc3705aa0a5c2d639d80e70edd0be8b0e553121
JustAgust <charles.sparks@atlasschool.com>
2024-02-10 22:40:46 +0000

    stop count and print

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
- * argc_argv
- * @c:
- *
- *
+ * argc_argv - takes input from argc and prints new name new line
+ * @argc: argument count
+ * @argv: array containing the string
+ * @i: input string
+ * Return: 0
  */
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
 	int i = 0;
 
