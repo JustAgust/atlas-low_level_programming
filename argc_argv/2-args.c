20a5395d48f3087416d11e39419cfc6f4df690fd
JustAgust <charles.sparks@atlasschool.com>
2024-02-11 22:34:34 +0000

    arguments recieved

---
+++ argc_argv/2-args.c
@@ -0,0 +1,12 @@
+include <stdio.h>
+/** main - prints all args received
+ * @argc: argument counter
+ * @argv: string of args
+ *Return: 0 = success
+ */
+int main(int argc, char **argv)
+{
+	while (argc--)
+		printf("%s\n", *argv++);
+	return (0);
+}
