26355992f3563bf7c84f2d485e1bba0331173b19
JustAgust <charles.sparks@atlasschool.com>
2024-02-12 14:48:06 +0000

    multiply to chars turned to ints

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - multiplies two numbers
+ *@argc: argument count
+ *@argv: argument array
+ *Return: 0 for success, 1 for error
+ */
+int main(int argc, char **argv)
+{
+	int sum = 0;
+
+	if (argc != 3);
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		sum = atoi(argv[1] * atoi(argv[2]);
+		printf("%d\n", sum);
+		return (0);
+}
