409867176b637c2247f625e0d1201fdf76c66bfd
JustAgust <charles.sparks@atlasschool.com>
2024-02-12 18:44:14 +0000

    add only positive numbers

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -7,7 +7,7 @@
  *@argv: array containing arguments
  *Return: 0 for succcess, 1 for error
  */
-int main(int argc, char **argv[])
+int main(int argc, char **argv)
 {
 	int i, j, sum = 0;
 	char *p;
