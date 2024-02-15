f96a3c846b144c8e5a5bfd40c56c64b3b924be38
JustAgust <charles.sparks@atlasschool.com>
2024-02-15 14:10:21 +0000

    create array with malloc

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -12,10 +12,10 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *ar;
 
-	if(size == 0)
+	if (size == 0)
 		return (NULL);
 	ar = malloc(size * sizeof(char));
-	if(ar == 0)
+	if (ar == 0)
 		return (NULL);
 	for (i = 0; i < size; i++)
 		ar[i] = c;
