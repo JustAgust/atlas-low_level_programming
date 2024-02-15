7e3cd76211e1a453d2bc0d92b95c7f9d50f029c3
JustAgust <charles.sparks@atlasschool.com>
2024-02-15 19:12:28 +0000

    2D grid

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -2,42 +2,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
-* alloc_grid - returns pointer of 2D array
-* @width: row of array
-* @height: column of array
-* Return: returns 0
-*/
+ * alloc_grid - returns pointer of 2D array
+ * @width: row of array
+ * @height: column of array
+ * Return: returns 0
+ */
 int **alloc_grid(int width, int height)
 {
 	int x, y;
-        int **ar;
+	int **ar;
 
 	if (width <= 0 || height <= 0)
 	{
-	return (NULL);
+		return (NULL);
 	}
 	ar = malloc(sizeof(int *) * height);
 	if (ar == NULL)
 	{
-		 free(ar);
-	return (NULL);
+		free(ar);
+		return (NULL);
 	}
 	for (y = 0; y < height; y++)
 	{
-		 ar[y] = malloc(sizeof(int) * width);
-		 if (ar[y] == NULL)
-		 {
-			 for (y--; y >= 0; y--)
-			 {
-				 free(ar[y]);
-			 }
-			 free(ar);
-			 return (NULL);
-		 }
-		 for (x = 0; x < width; x++)
-		 {
-			 ar[y][x] = 0;
-		 }
-	 }
-	 return (ar);
- }
+		ar[y] = malloc(sizeof(int) * width);
+		if (ar[y] == NULL)
+		{
+			for (y--; y >= 0; y--)
+			{
+				free(ar[y]);
+			}
+			free(ar);
+			return (NULL);
+		}
+		for (x = 0; x < width; x++)
+		{
+			ar[y][x] = 0;
+		}
+	}
+	return (ar);
+}
