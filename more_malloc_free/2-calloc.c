ef8db50ede7eb28d1782973a57d4055710712324
JustAgust <charles.sparks@atlasschool.com>
2024-02-17 22:54:42 +0000

    mem for array

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -17,8 +17,8 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (size == 0)
 		return (NULL);
 	ptr = malloc(nmemb * size);
-       if (ptr == NULL)
-       return (NULL);	       
+	if (ptr == NULL)
+		return (NULL);
 	for (i = 0; i < (nmemb * size); i++)
 		ptr[i] = 0;
 	return (ptr);
