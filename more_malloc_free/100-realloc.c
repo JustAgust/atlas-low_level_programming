d3234d5db7ec3e7df5df7ba6f5afc5e3c507e2e9
JustAgust <charles.sparks@atlasschool.com>
2024-02-19 23:36:15 +0000

    bonus 1

--- more_malloc_free/100-realloc.c
+++ more_malloc_free/100-realloc.c
@@ -4,37 +4,37 @@
 #include <string.h>
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
-{ // declaring vars
+{ /* declaring vars */
 unsigned int index;
 char *newpoint;
 char *optr = ptr;
-//if newsize is zero free pointer return null
+/*if newsize is zero free pointer return null*/
 if(new_size == 0)
 {
 free(ptr);
 return(NULL);
 }
-//if equal do nothing return
+/*if equal do nothing return */
 if (new_size == old_size)
 {
 return (ptr);
 }
-// allocate new mem
+/* allocate new mem */
 newpoint = malloc(new_size);
-//if ptr is null
+/*if ptr is null*/
 if(ptr == NULL)
 {
     return (newpoint);
 }
 
-// copy old to new
+/*copy old to new*/
 else
 {
 
 for (index = 0; index < old_size; index++)
 newpoint[index] = optr[index];
 
-// free old
+/* free old */
     free (ptr);
     return (newpoint);
     }
