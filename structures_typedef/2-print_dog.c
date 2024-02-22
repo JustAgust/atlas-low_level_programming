656e85b11e1682e36827d4a939dfc9011558c940
JustAgust <charles.sparks@atlasschool.com>
2024-02-22 20:27:34 +0000

    2dog

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+/**
+ *@d: elements to print unless null
+ *Return: no return
+ *@s: value to check if null
+ */
+char *check_null(char *s)
+{
+	if (s == NULL)
+		return (nil);
+	else
+		return (s);
+}
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: %s\n", check_null(d->name));
+		printf("Age: %.2f\n", d->age);
+		printf("Owner: %s\n", check_null(d->owner));
+	}
+}
+
+
