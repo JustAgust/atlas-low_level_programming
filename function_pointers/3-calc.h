8a8d8079ea0bab8a2a9655809b74eda505799fc4
JustAgust <charles.sparks@atlasschool.com>
2024-02-24 22:50:48 +0000

    changed 3-calc.h

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -1,3 +1,7 @@
+#ifndef CALC_H
+#define CALC_H
+
+
 #include <stdio.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -17,8 +21,6 @@ typedef struct op
 /**
  * prototypes
  */
-#ifndef CALC_H
-#define CALC_H
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
