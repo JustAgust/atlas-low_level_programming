0210e728836fc087989583fe0d7abdcb47eabe75
JustAgust <charles.sparks@atlasschool.com>
2024-02-24 20:03:02 +0000

    task3 wow!

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,29 @@
+#include "3-calc.h"
+/**
+ *
+ *
+ *
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	int i;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
+	while (ops[i].op != NULL)
+	{
+		if (ops[i].op == s)
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}
+
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,53 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ *op_add - returns sum of two ints
+ *@a: first int
+ *@b: second int
+ *Return: returns sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ *
+ *
+ *
+ *
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ *
+ *
+ *
+ *
+ */
+int op_mul(int a, int b)
+{
+        return (a * b);
+}
+/**
+ *
+ *
+ *
+ *
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ *
+ *
+ *
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
