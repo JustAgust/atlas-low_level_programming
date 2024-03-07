fba18a6fb11321783fed32e421d6b3296b6b7339
JustAgust <charles.sparks@atlasschool.com>
2024-03-07 17:43:53 +0000

    duplicate string

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * add_node - adds new node to beginning
+ * @head: a pointer that points to list
+ * @str: the string to be duplicated
+ * Return: A list_t type list
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *temp;
+
+	if (head == NULL || str == NULL)
+		return (NULL);
+
+	temp = malloc(sizeof(list_t));
+	if (temp == NULL);
+	{
+		free(temp);
+		return (NULL);
+	}
+	temp->str = strdup(str);
+	if (temp->str == NULL);
+	{
+		free(temp);
+		return (NULL);
+	}
+	temp->len = strlen(str);
+	temp->next = *head;
+	*head = temp;
+
+	return (temp);
+}
