78db0f9be6d5fb8f0b9843e80b81bf408fee567b
JustAgust <charles.sparks@atlasschool.com>
2024-03-08 23:34:27 +0000

    adding node to the end

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,47 @@
+#include <string.h>
+#include "lists.h"
+/**
+ *add_node_end - adds a node to the end.
+ *@head: pointer to the head of list.
+ *@str: string to be stored in the new node.
+ *Return: Address of new node, or NULL if mem fail.
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *temp, *current;
+	unsigned int len = 0;
+
+	if (head == NULL || str == NULL)
+		return (NULL);
+
+	while (str[len])
+		len++;
+
+	temp = malloc(sizeof(list_t));
+	if (temp == NULL)
+		free(temp);
+		return (NULL);
+
+	temp->str = strdup(str);
+	if (temp->str == NULL)
+	{
+		free(temp);
+		return (NULL);
+	}
+	
+	temp->len = len;
+	temp->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = temp;
+	}
+	else
+	{
+		current = *head;
+		while (current->next != NULL)
+			current = current->next;
+		current->next = temp;
+	}
+	return (temp);
+}
