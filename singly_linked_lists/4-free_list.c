5082d24afc2fe991453bb1570112ade3f4cebbd7
JustAgust <charles.sparks@atlasschool.com>
2024-03-09 23:56:53 +0000

    frees the list_t list

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_list - frees list_t
+ * @head: points to beginning of list
+ * Return: nothing
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
