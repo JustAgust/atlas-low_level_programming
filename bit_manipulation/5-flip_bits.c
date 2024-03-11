ed2a098dc332876a702ec08e91a48418809a63fb
JustAgust <charles.sparks@atlasschool.com>
2024-03-11 19:31:00 +0000

    flip bits

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * flip_bits - calculates bits to flip to get from one number to another number
+ * @n: first input number to be converted
+ * @m: second input number to be converted
+ * Return: number of bits needed 
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int x = n ^ m;
+	unsigned int count = 0;
+
+	if (x == 0)
+		return (0);
+	while (x > 0)
+	{
+		x = x &(x - 1);
+		count++;
+	}
+	return (count);
+}
