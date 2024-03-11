c3657bd9f9be8d2d8b1be154d0da76a210064d79
JustAgust <charles.sparks@atlasschool.com>
2024-03-11 18:57:41 +0000

    turn bit val into decimal

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * get_bit - returns value of given bit at index
+ * @index: is index
+ * Return: returns value of index or -1 if error
+ */
+int get_bit(unsigned long int, insigned int index)
+{
+	int bit_val;
+
+	if (index > 63)
+		return (-1);
+
+	bit_val = (n >> index) & 1;
+
+	return (bit_val);
+}
