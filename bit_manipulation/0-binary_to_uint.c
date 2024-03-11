3797226452ed0c1ed14864b30ffe5044c4a0d10a
JustAgust <charles.sparks@atlasschool.com>
2024-03-11 18:45:01 +0000

    convert binary to int 3rd try

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -5,21 +5,26 @@
  * @b: points to string of 0 and 1 chars
  * Return: returns converted number or 0 if empty or another number besides 0,1
  */
-unsigned int binary_to_unit(const char *b)
+unsigned int binary_to_uint(const char *b)
 {
-	unsigned int result = 0;
-	int i;
+	long int result = 0, bit;
+	int i, j = 0;
 
 	if (b == NULL)
-		return 0;
-
+		return (0);
 	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
-
-		result = (result << 1) | (b[i] - '0');
 	}
-
+	i--;
+	for (; i >= 0; i--, j++)
+	{
+		if (b[i] == '0')
+			bit = 0;
+		else
+			bit = 1;
+		result = result + (bit << j);
+	}
 	return (result);
 }
