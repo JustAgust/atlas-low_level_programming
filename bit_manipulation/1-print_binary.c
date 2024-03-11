38cf30e3e2e4844ffa16b61a9f2f45159100facd
JustAgust <charles.sparks@atlasschool.com>
2024-03-11 18:52:26 +0000

    print binary

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_binary - call a sub function to print the binary number
+ * @n: input number
+ * Return: nothing
+ */
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+	{
+		print_binary(n >> 1);
+	}
+	_putchar((n & 1) + 48);
+}
