8451c990f42de8455a95f7f6fb67756f6309861c
JustAgust <charles.sparks@atlasschool.com>
2024-03-11 19:41:40 +0000

    I have no clue how to do this

---
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+/**
+ * delete_nodeint_at_index - deletes node at index
+ * @head: pointer that points to beginning of list
+ * @index: int to be deleted
+ * Return: 1 if succes, -1 if fail
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	{
+	unsigned int i = 0;
+	listint_t *node, *nodelist;
+
+	if (*head == NULL)
+	{
+		return (-1);
+	}
+	nodelist = *head;
+	if (index == 0)
+	{
+		nodelist = (*head)->next;
+		free(*head);
+		*head = nodelist;
+		return (1);
+	}
+	while (i < index && nodelist->next != NULL)
+	{
+		node = nodelist;
+		nodelist = nodelist->next;
+		i++;
+	}
+	if (i < index)
+	{
+		return (-1);
+	}
+	node->next = nodelist->next; 
+	free(nodelist);
+	return (1);
+}
