2d69a78504651cd7958f0a7f6ff6ddb23c05dc53
JustAgust <charles.sparks@atlasschool.com>
2024-03-11 19:35:26 +0000

    I have no clue how to do this

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -9,4 +9,41 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int indx, int n)
 {
+	listint_t *nodelist, *node;
+	unsigned int i = 0;
 
+	node = malloc(sizeof(listint_t));
+	if (node == NULL)
+	{
+		free(node);
+		return (NULL);
+	}
+	if (*head == NULL)
+	{
+		node->n = n;
+		node->next = *head;
+		*head = node;
+		return (node);
+	}
+	if (idx == 0) /*node inserts in the beginning*/
+	{
+		node->n = n;
+		node->next = *head;
+		*head = node;
+		return (node);
+	}
+	nodelist = *head;
+	while (i < idx - 1 && nodelist != NULL)
+	{
+		nodelist = nodelist->next;
+		i++;
+	}
+	if (i < idx - 1) /*idx is longer than nodelist length*/
+	{
+		return (NULL);
+	}
+	node->n = n;
+	node->next = nodelist->next;
+	nodelist->next = node;
+	return (node);
+}
