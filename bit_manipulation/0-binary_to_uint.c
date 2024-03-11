5c847594f87a2154e1792e62aab850d4813bde8c
JustAgust <charles.sparks@atlasschool.com>
2024-03-11 18:38:07 +0000

    convert binary to int

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * binary_to_unit - convert binary to unsigned int
+ * @b: points to string of 0 and 1 chars
+ * Return: returns converted number or 0 if empty or another number besides 0,1
+ */
+unsigned int binary_to_unit(const char *b)
+{
+	unsigned int result = 0;
+	int i;
+
+	if (b == NULL)
+		return 0;
+
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+
+		result = (resutl << 1) | (b[i] - '0');
+	}
+
+	return (result);
+}
