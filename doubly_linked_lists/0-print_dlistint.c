48fa3c99451d5f98cfbc71d120661ee18c1c99b4
JustAgust <charles.sparks@atlasschool.com>
2024-03-15 17:21:05 +0000

    code style issues

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -6,13 +6,12 @@
 */
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t i = 0;
-
-    while (h != NULL)
-    {
-    i++;
-    printf("%d\n", h->n);
-    h = h->next;
-    }
-    return (i); 
+size_t i = 0;
+while (h != NULL)
+{
+i++;
+printf("%d\n", h->n);
+h = h->next;
+}
+return (i);
 }
--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -6,12 +6,12 @@
 */
 size_t dlistint_len(const dlistint_t *h)
 {
-    size_t i = 0;
-    
-    while (h != NULL)
-    {
-    i++;
-    h = h->next;
-    }
-    return (i);
+size_t i = 0;
+
+while (h != NULL)
+{
+i++;
+h = h->next;
+}
+return (i);
 }
