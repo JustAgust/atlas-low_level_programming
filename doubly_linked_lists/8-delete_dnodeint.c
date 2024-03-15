9e4ba97effbe038cbdda448327ee2703d81836eb
JustAgust <charles.sparks@atlasschool.com>
2024-03-15 21:04:46 +0000

    not as difficult but seriously

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -7,11 +7,11 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	if (*head == NULL)
-		return (-1);
-
 	dlistint_t *current = *head;
 	unsigned int count = 0;
+
+	if (*head == NULL)
+		return (-1);
 
 	while (current != NULL && count < index)
 	{
