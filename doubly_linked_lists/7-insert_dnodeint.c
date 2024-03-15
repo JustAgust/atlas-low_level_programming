8aafabaf65d3cd806d4e2d1c95458f09c116b967
JustAgust <charles.sparks@atlasschool.com>
2024-03-15 20:25:49 +0000

    seems like these get suddenly difficult out of nowhere! wow.

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,47 @@
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index - inserts node at a given position
+ * @h: pointer to head of dlistint
+ * @idx: given index
+ * @n: int value added to new node
+ * Return: address of new node, or NULL if failed
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new_node, *current = *head;
+	unsigned int count = 0;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = NULL;
+
+	if (idx == 0)
+		{
+			new_node->next = *head;
+			if (*head != NULL)
+				(*head)->prev = new_node;
+			*head = new_node;
+			return (NULL);
+		}
+	while (current != NULL && count < idx - 1)
+	{
+		current = current->next;
+		count++;
+	}
+
+	if (current == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->next = current->next;
+	new_node->prev = current;
+	if (current->next != NULL)
+		current->next->prev = new_node;
+	current->next = new_node;
+	return (new_node);
+}
