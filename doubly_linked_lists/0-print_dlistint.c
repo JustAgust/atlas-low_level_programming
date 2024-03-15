a6c30ca6a97092182c7e4b88e4d1f14875863c78
JustAgust <charles.sparks@atlasschool.com>
2024-03-15 16:10:50 +0000

    Print list

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+*print_dlistint - prints all the elements of the list
+*@h: doubly linked list
+*Return: number of nodes
+*/
+size_t print_dlsitint(const dlistint_t *h)
+{
+    size_t i = 0;
+    while (h != NULL)
+    i++;
+    printf("%d\n" h->n)
+    h = h->next;
+    return (i); 
+}
