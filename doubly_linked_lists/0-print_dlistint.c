2a8a23c9514b6d04daa52df3984df5c26c3f0811
JustAgust <charles.sparks@atlasschool.com>
2024-03-15 17:30:32 +0000

    trying to fix code style

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,17 +1,17 @@
 #include "lists.h"
 /**
-*print_dlistint - prints all the elements of the list
-*@h: doubly linked list
-*Return: number of nodes
-*/
+ *print_dlistint - prints all the elements of the list
+ *@h: doubly linked list
+ *Return: number of nodes
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
-size_t i = 0;
-while (h != NULL)
-{
-i++;
-printf("%d\n", h->n);
-h = h->next;
-}
-return (i);
+	size_t i = 0;
+	while (h != NULL)
+	{
+		i++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (i);
 }
--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,17 +1,17 @@
 #include "lists.h"
 /**
-*dlistint_len - returns number of elements
-*@h: doubly linked list
-*Return: element count
-*/
+ *dlistint_len - returns number of elements
+ *@h: doubly linked list
+ *Return: element count
+ */
 size_t dlistint_len(const dlistint_t *h)
 {
-size_t i = 0;
+	size_t i = 0;
 
-while (h != NULL)
-{
-i++;
-h = h->next;
-}
-return (i);
+	while (h != NULL)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
 }
