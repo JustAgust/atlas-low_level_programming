b7d2abb9dd408daf2b8c545d610dcd2afcde56f8
JustAgust <charles.sparks@atlasschool.com>
2024-03-15 20:31:37 +0000

    way too difficult suddenly

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -25,7 +25,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			if (*h != NULL)
 				(*h)->prev = new_node;
 			*h = new_node;
-			return (NULL);
+			return (new_node);
 		}
 	while (current != NULL && count < idx - 1)
 	{
