32708d602abe65551e353dff61fdb1d7d0fd796f
JustAgust <charles.sparks@atlasschool.com>
2024-03-15 19:59:38 +0000

    sum up the data in the nodes

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * sum_dlistint - returns sum of all the data of the dlist
+ * @head: pointer to head of dlistint_t
+ * Return: 0
+ */
+int sum_dlistint(dlistint_t *head);
+{
+	int sum = 0;
+	dlistint_t *current = head;
+
+	while (current != NULL)
+	{
+		sum += current->next;
+		current = current->next;
+	}
+	return (sum);
+}
