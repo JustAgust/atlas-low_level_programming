dd46354c6774c14e33e63c83301915747b622c42
JustAgust <charles.sparks@atlasschool.com>
2024-03-15 19:33:51 +0000

    free at last

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_dlistint - frees the list!
+ * @head: pointer to head of list, where to begin the attack!
+ * Return: Success! technically returns nothing which is success.
+ */
+void free_dlistint(dlist_t *head)
+{
+	dlistint_t *current;
+
+	while (head != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current)
+	}
+}
