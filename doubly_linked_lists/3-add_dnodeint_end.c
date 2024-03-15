679e0e72ebc8b590a9cdcab4d56782ac3405a9e2
JustAgust <charles.sparks@atlasschool.com>
2024-03-15 19:24:24 +0000

    add node to end

---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+/**
+ * add_dnodeint_end - adds node to end of list
+ * @head: start of linked list
+ * @n: interger value to add
+ * Return: address of new element or NULL if failed
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node, *current_node;
+	
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		new_node->prev = NULL;
+		*head = new_node;
+	}
+	else
+	{
+		current = *head;
+		while (current->next != NULL)
+			current = current->next;
+
+		current->next = new_node;
+		new_node->prev = current;
+	}
+	return (new_node);
+}
