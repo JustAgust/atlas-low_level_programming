7248697d31db4569b22ba763718d14baccf6148c
JustAgust <charles.sparks@atlasschool.com>
2024-03-15 16:17:51 +0000

    forgot comma in printf

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -9,7 +9,7 @@ size_t print_dlsitint(const dlistint_t *h)
     size_t i = 0;
     while (h != NULL)
     i++;
-    printf("%d\n" h->n);
+    printf("%d\n", h->n);
     h = h->next;
     return (i); 
 }
