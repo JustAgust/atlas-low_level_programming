8a1aa913ff4ca4e930efcafd1c7bc419216ba4a6
JustAgust <charles.sparks@atlasschool.com>
2024-03-15 19:10:25 +0000

    fixed trailing white space

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -8,7 +8,7 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
-	
+
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
@@ -19,11 +19,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new->next = *head;
 
 	if (*head != NULL)
-	
-		(*head)->prev = new;
-		
-	*head = new;	
 
-	
+		(*head)->prev = new;
+
+	*head = new;
+
+
 	return (new);
 }
