244c2565e3fc6c5b4c025544ebb81ff599b7c2c6
JustAgust <charles.sparks@atlasschool.com>
2024-03-15 20:55:24 +0000

    not as difficult but seriously

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -22,7 +22,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (current == NULL)
 		return (-1);
 
-	if (current->next != NULL)
+	if (current->prev != NULL)
 		current->prev->next = current->next;
 	else
 		*head = current->next;
